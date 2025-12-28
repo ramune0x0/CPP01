cefc4424383ff2a366181597c7fc6a40d12310a4
hminemur <h.minemura.0x@gmail.com>
2025-12-28 12:00:26 +0900

    ex01

---
+++ ex01/Zombie.cpp
@@ -0,0 +1,28 @@
+#include "Zombie.hpp"
+
+// Constructor
+Zombie::Zombie(std::string name) : name(name){
+}
+
+// Destructor
+Zombie::~Zombie(){
+    std::cout << name << " is destroyed" << std::endl;
+}
+
+// set zombie name
+
+void Zombie::setName(std::string& setname){
+    name = setname;
+}
+
+// Zombies announce themselves as follows:
+// <name>: BraiiiiiiinnnzzzZ...
+
+// Do not print the angle brackets (< and >). 
+// For a zombie named Foo, the message would be:
+// Foo: BraiiiiiiinnnzzzZ...
+
+void Zombie::announce(void){
+    std::cout << name << " : BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
