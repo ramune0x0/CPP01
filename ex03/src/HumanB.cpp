17580860aae160f8e33ef0f04a0f48cdde5ab39e
haru minemura <h.minemura.0x@gmail.com>
2026-01-03 21:52:17 +0900

    ex03

---
+++ ex03/src/HumanB.cpp
@@ -0,0 +1,15 @@
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string name) : name(name), weapon(NULL) {}
+
+void HumanB::setWeapon(Weapon &weapon) {
+    this->weapon = &weapon;
+}
+
+void HumanB::attack() {
+    if (weapon != NULL) {
+        std::cout << name << " attacks with their " << weapon->getType() << std::endl;
+    } else {
+        std::cout << name << " has no weapon" << std::endl;
+    }
+}
